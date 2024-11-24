0464e08e7827f49236e2327176258f1ec9308663
Bonda Catalin <bondacatalin3@outlook.com>
2024-11-24 23:31:41 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace BondaCatalinLab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		
+	}
+}
---
+++ NewPage1.xaml.cs
@@ -0,0 +1,9 @@
+namespace BondaCatalinLab7;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
